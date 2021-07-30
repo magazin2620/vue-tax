d2b8eadce9c9814e3b66de6679b2c46c8193e0a6
magazin2620 <69750938+magazin2620@users.noreply.github.com>
2021-07-30 18:49:29 +0300

    new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,112 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+  <v-container>
+    <v-row>
+      <v-col class="d-flex justify-center text-body-2" cols="12">
+        <v-sheet
+          class="d-flex justify-center align-center"
+          color="grey lighten-4"
+          elevation="7"
+          min-height="420"
+          min-width="420"
+          max-width="550"
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+          <v-sheet
+            class="pa-5 ma-4"
+            elevation="2"
+            min-height="400"
+            min-width="400"
+            max-width="530"
+          >
+            <div class="text-subtitle-1 font-weight-medium">
+              Добавление организации
+            </div>
+            <v-row class="d-flex align-center">
+              <v-col cols="3">Организация</v-col>
+              <v-col cols="9">
+                <v-autocomplete
+                  :items="items"
+                  clearable
+                  :search-input.sync="search"
+                  item-text="value"
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+            <div class="text-subtitle-1 font-weight-medium mb-5">Реквизиты</div>
+            <v-row class="d-flex align-top">
+              <v-col cols="3">Название</v-col>
+              <v-col cols="9" v-for="(item, i) in items" :key="i">{{
+                item.value
+              }}</v-col>
+            </v-row>
+            <v-row class="d-flex align-center">
+              <v-col cols="3">ИНН</v-col>
+              <v-col cols="9" v-for="(item, i) in items" :key="i">{{
+                item.inn
+              }}</v-col>
+            </v-row>
+            <v-row class="d-flex align-center">
+              <v-col cols="3">КПП</v-col>
+              <v-col cols="9" v-for="(item, i) in items" :key="i">{{
+                item.kpp
+              }}</v-col>
+            </v-row>
+            <v-row class="d-flex align-center">
+              <v-col cols="3">ОГРН</v-col>
+              <v-col cols="9" v-for="(item, i) in items" :key="i">{{
+                item.ogrn
+              }}</v-col>
+            </v-row>
+            <v-row class="d-flex align-top">
+              <v-col cols="3">Юр. адрес</v-col>
+              <v-col cols="9" v-for="(item, i) in items" :key="i">{{
+                item.address
+              }}</v-col>
+            </v-row>
+            <div>{{ this.items }}</div>
+          </v-sheet>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      items: [],
+      search: null,
+    };
+  },
+  watch: {
+    search() {
+      axios
+        .get(
+          "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party?Content-Type=application/json&token=5de7b238a1fdc912edd8b15084d44da035ff59ff&query=" +
+            this.search
+        )
+        .then((response) => {
+          this.items = response.data.suggestions.map((item) => ({
+            value: item.value,
+            inn: item.data.inn,
+            kpp: item.data.kpp,
+            ogrn: item.data.ogrn,
+            address: item.data.address.unrestricted_value,
+          }));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.v-autocomplete label {
+  font-size: 0.8em;
+  margin-left: 5px;
 }
 </style>
